8bbba85b15da834bc2f10b5d8f397b71078224b2
Dokakoka <Dokakoka>
2022-08-31 17:46:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+#  dbwrapper 
+
+dbwrapper is a small php wrapper for mysql databases.
+
+## installation
+
+install once with composer:
+
+```
+composer require ahmed/db-wrapper
+```
+
+then add this to your project:
+
+```php
+require __DIR__ . '/vendor/autoload.php';
+use Ahmed\DbWrapper\db;
+$db = new db();
+```
+
+## usage
+
+```php
+/* connect to database */
+$db = new db('127.0.0.1', 'username', 'password', 'database', 3306);
+
+/* insert/update/delete */
+$id = $db->insert('tablename', ['col1' => 'foo'])->excute();
+$db->update('tablename', ['col1' => 'bar'], ['id' => $id])->excute();
+$db->delete('tablename', ['id' => $id])->excute();
+
+/* select */
+$db->select("*", 'tablename', ['id' => $id]);
+
+```
---
+++ src/dbwrapper.php
@@ -0,0 +1,61 @@
+<?php
+namespace Ahmed\DbWrapper;
+class db {
+    public $connection;
+    public $sql;
+
+    public function __construct($host, $user, $password, $database, $port=3306) {
+        $this->connection = mysqli_connect($host, $user, $password, $database, $port);
+    }
+
+    public function select($columns, $table, $condition){
+        $query = mysqli_query($this->connection, "SELECT $columns FROM $table Where $condition");
+        while($row = mysqli_fetch_assoc($query)) {
+            $data[]= $row;
+        }
+        return $data;
+    }
+
+    public function insert($table, $data) {
+        $columns="";
+        $values="";
+        foreach ($data as $key =>$value) {
+            $columns .= "$key,";
+            $values.="'$value',";
+        }
+        $columns = rtrim($columns, ",");
+        $values = rtrim($values, ",");
+
+        $this->sql = "INSERT INTO $table ($columns) Values ($values)";
+        return $this;
+    }
+
+    public function update($table, $data, $condition) {
+        $columns="";
+        $values="";
+        $query = "";
+        foreach ($data as $key =>$value) {
+            $columns = "$key";
+            $values="'$value'";
+            $query .= "$columns=$values, ";
+        }
+        $query = rtrim($query, ", ");
+
+        $this->sql = "Update $table SET $query Where $condition";
+        return $this;
+    }
+
+    public function delete($table, $condition) {
+        $this->sql = "DELETE FROM $table Where $condition";
+        return $this;
+    }
+
+    public function excute() {
+        mysqli_query($this->connection, $this->sql);
+        if(mysqli_affected_rows($this->connection)>0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
